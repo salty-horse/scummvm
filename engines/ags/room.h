bbc04a101224dc13c6a439f08974635ff11658c1
Ori Avtalion <ori@avtalion.name>
2012-03-27 22:30:14 +0200

    AGS: Init RoomObject::_cycling

--- engines/ags/room.h
+++ engines/ags/room.h
@@ -79,7 +79,7 @@ struct RoomRegion : public ScriptObject {
 #define OBJF_DELETED        0x40  // object has been deleted
 
 struct RoomObject : public ScriptObject, public Drawable {
-	RoomObject(AGSEngine *vm, uint id) : _vm(vm), _interaction(NULL), _flags(0), _id(id) { }
+	RoomObject(AGSEngine *vm, uint id) : _vm(vm), _interaction(NULL), _flags(0), _id(id), _cycling(0) { }
 	bool isOfType(ScriptObjectType objectType) { return (objectType == sotRoomObject); }
 	const char *getObjectTypeName() { return "RoomObject"; }
 
